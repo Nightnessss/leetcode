58e13f0bef9ee0bc1eaf573e297dfdc1ff961fa7
nightnessss <nightnessss@163.com>
2020-09-10 00:01:02 +0800

    feat(day20200909): Q39 组合总和

---
+++ day20200909/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/**
+给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的数字可以无限制重复被选取。
+
+说明：
+
+所有数字（包括 target）都是正整数。
+解集不能包含重复的组合。
+示例 1：
+
+输入：candidates = [2,3,6,7], target = 7,
+所求解集为：
+[
+  [7],
+  [2,2,3]
+]
+示例 2：
+
+输入：candidates = [2,3,5], target = 8,
+所求解集为：
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+
+
+提示：
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+candidate 中的每个元素都是独一无二的。
+1 <= target <= 500
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combination-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+	candidates := []int{7, 2, 3}
+	target := 18
+	res := combinationSum(candidates, target)
+	fmt.Print(res)
+}
+
+//
+//type A struct {
+//	res    [][]int
+//	target int
+//}
+
+func combinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	var h []int
+	var res [][]int
+	_, res = get(0, candidates, target, h, res)
+	return res
+}
+
+func get(sum int, arr []int, target int, h []int, res [][]int) ([]int, [][]int) {
+	if sum == target {
+		res = append(res, h)
+		h = []int{}
+		return h, res
+	}
+	if sum > target {
+		h = []int{}
+		return h, res
+	}
+	for i := len(arr) - 1; i >= 0; i-- {
+
+		if sum+arr[i] > target {
+			continue
+		}
+		if sum+arr[i] == target {
+			res = append(res, append(h, arr[i]))
+			continue
+		}
+		if sum+arr[i] < target {
+			tmp := append(h, arr[i])
+			_, res = get(sum+arr[i], arr[:i+1], target, tmp, res)
+		}
+	}
+	return h, res
+}
