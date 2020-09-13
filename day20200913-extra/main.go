c84a6dbd5bb3292a679287844d6afa97cc050e11
nightnessss <nightnessss@163.com>
2020-09-13 22:04:16 +0800

    feat(day20200913): Q547 朋友圈

---
+++ day20200913-extra/main.go
@@ -0,0 +1,108 @@
+package main
+
+import "fmt"
+
+/**
+班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+
+给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+
+
+
+示例 1：
+
+输入：
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+输出：2
+解释：已知学生 0 和学生 1 互为朋友，他们在一个朋友圈。
+第2个学生自己在一个朋友圈。所以返回 2 。
+示例 2：
+
+输入：
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+输出：1
+解释：已知学生 0 和学生 1 互为朋友，学生 1 和学生 2 互为朋友，所以学生 0 和学生 2 也是朋友，所以他们三个在一个朋友圈，返回 1 。
+
+
+提示：
+
+1 <= N <= 200
+M[i][i] == 1
+M[i][j] == M[j][i]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/friend-circles
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+	input := [][]int{
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+		{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+		{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}}
+
+	res := findCircleNum(input)
+	fmt.Print(res)
+}
+
+type AndCollect struct {
+	collect []int
+}
+
+func (c *AndCollect) find(x int) int {
+	if c.collect[x] == x {
+		return x
+	}
+	c.collect[x] = c.find(c.collect[x])
+	return c.collect[x]
+}
+
+func (c *AndCollect) join(x int, y int) {
+	xroot := c.find(c.collect[x])
+	yroot := c.find(c.collect[y])
+	if xroot != yroot {
+		c.collect[xroot] = yroot
+	}
+}
+
+func findCircleNum(M [][]int) int {
+	count := 0
+	c := &AndCollect{
+		collect: make([]int, len(M)),
+	}
+	for i := 0; i < len(M); i++ {
+		c.collect[i] = i
+	}
+	for i, m := range M {
+		for j, n := range m {
+			if j <= i {
+				continue
+			}
+			if n == 1 {
+				c.join(i, j)
+			}
+		}
+	}
+	for i, q := range c.collect {
+		if i == q {
+			count++
+		}
+	}
+	return count
+}
