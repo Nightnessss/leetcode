9ca58aba634b290a616d7bdf9145e629140a9af4
Nightnessss <nightnessss@163.com>
2020-09-30 11:44:21 +0800

    feat(day20200930): Q701 二叉搜索树中的插入操作

---
+++ day20200930/Main.java
@@ -0,0 +1,100 @@
+package day20200930;
+
+/**
+ * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据保证，新值和原始二叉搜索树中的任意节点值都不同。
+ * <p>
+ * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。
+ * <p>
+ *  
+ * <p>
+ * 例如, 
+ * <p>
+ * 给定二叉搜索树:
+ * <p>
+ * 4
+ * / \
+ * 2   7
+ * / \
+ * 1   3
+ * <p>
+ * 和 插入的值: 5
+ * 你可以返回这个二叉搜索树:
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   /
+ * 1   3 5
+ * 或者这个树也是有效的:
+ * <p>
+ * 5
+ * /   \
+ * 2     7
+ * / \
+ * 1   3
+ * \
+ * 4
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 给定的树上的节点数介于 0 和 10^4 之间
+ * 每个节点都有一个唯一整数值，取值范围从 0 到 10^8
+ * -10^8 <= val <= 10^8
+ * 新值和原始二叉搜索树中的任意节点值都不同
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insert-into-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author weirwei 2020/9/30 10:54
+ */
+public class Main {
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
+        TreeNode tmp = root;
+        while (true) {
+
+            if (tmp.val < val) {
+                if (tmp.right == null) {
+                    tmp.right = new TreeNode(val);
+                    break;
+                }
+                tmp = tmp.right;
+                continue;
+            }
+            if (tmp.left == null) {
+                tmp.left = new TreeNode(val);
+                break;
+            }
+            tmp = tmp.left;
+        }
+        return root;
+    }
+}
