afdfa4decf059b6be2549bb4d1a9b618bd2d26bf
Nightnessss <nightnessss@163.com>
2020-10-02 15:20:10 +0800

    feat(day20201002): Q771 宝石与石头

---
+++ day20201002/Main.java
@@ -0,0 +1,56 @@
+package day20201002;
+
+import java.util.HashMap;
+
+/**
+ *  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ * <p>
+ * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: J = "aA", S = "aAAbbbb"
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: J = "z", S = "ZZ"
+ * 输出: 0
+ * 注意:
+ * <p>
+ * S 和 J 最多含有50个字母。
+ *  J 中的字符不重复。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jewels-and-stones
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author weirwei 2020/10/2 13:20
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String J = "aA", S = "aAAbbbb";
+        Solution solution = new Solution();
+        int res = solution.numJewelsInStones(J, S);
+        System.out.println(res);
+    }
+
+}
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        char[] charsJ = J.toCharArray();
+        char[] charsS = S.toCharArray();
+        for (char j : charsJ) {
+            hashMap.put(j, 0);
+        }
+        int sum = 0;
+        for (char s : charsS) {
+            if (hashMap.containsKey(s)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
