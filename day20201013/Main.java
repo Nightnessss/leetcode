fea9b841fdd43a05d7e1ad470d1abc71e880be48
Nightnessss <nightnessss@163.com>
2020-10-13 23:37:03 +0800

    feat(day20201013): Q24 两两交换链表中的节点

---
+++ day20201013/Main.java
@@ -0,0 +1,77 @@
+package day20201013;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ *
+ * 输入：head = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *  
+ *
+ * 提示：
+ *
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author weirwei 2020/10/13 22:56
+ */
+public class Main {
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode tmp = head;
+        ListNode h = new ListNode(-1, head);
+        head = h;
+        while (tmp != null) {
+            ListNode p = tmp;
+            tmp = tmp.next;
+            if (tmp != null) {
+                p.next = tmp.next;
+                tmp.next = p;
+                h.next = tmp;
+                h = p;
+                tmp = p.next;
+            }
+        }
+        return head.next;
+    }
+}
