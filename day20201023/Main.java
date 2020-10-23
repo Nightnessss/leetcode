382310208b7842c0fee6d539fd7782f460ab9a62
Nightnessss <nightnessss@163.com>
2020-10-23 23:26:44 +0800

    feat(day20201023): Q234 回文链表

---
+++ day20201023/Main.java
@@ -0,0 +1,56 @@
+package day20201023;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ *
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author weirwei 2020/10/23 23:25
+ */
+public class Main {
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (j > i) {
+            if (list.get(i).equals(list.get(j))) {
+                i++;
+                j--;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
