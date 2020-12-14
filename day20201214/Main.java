42fc1a62c4de2d785c91bd9c239386343afc0fc6
Nightnessss <nightnessss@163.com>
2020-12-14 14:47:50 +0800

    feat(day20201214): Q49 字母异位词分组

---
+++ day20201214/Main.java
@@ -0,0 +1,50 @@
+package day20201214;
+
+import java.util.*;
+
+/**
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * 说明：
+ *
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author weirwei 2020/12/14 14:04
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] strs = new String[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(solution.groupAnagrams(strs));
+    }
+}
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
